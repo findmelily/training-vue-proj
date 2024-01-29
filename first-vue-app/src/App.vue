e2fd952dfbf6ed05e3b0b153605621dbe43ce6b4
findmelily <pinny.pinngam@gmail.com>
2024-01-29 15:27:29 +0700

    my commit

---
+++ first-vue-app/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+  //composition api
+  import { ref } from 'vue' //สร้างตัวแปรผูกกับ DOM
+  //ref function will creates a reactive varriable when it changes, DOM change automatically
+  const count = ref(0)
+  const decreaseCount = () => {
+    count.value === 0 ? count.value = 0 : --count.value
+  }
+  const isDark = ref(false)
+</script>
+ 
+<template>
+  <div class="min-h-screen" :class="isDark ? 'bg-black' : 'bg-white'" >
+    <div class="flex justify-end p-2">
+    <input type="checkbox" class="toggle" v-model="isDark" @click="isDark = !isDark" />
+    </div>
+  
+  
+  <div>
+    <h1 class="text-green-500 text-2xl font-semibold tracking-widest">
+      My First Counter Application
+    </h1>
+  </div>
+  <div class="flex p-4 space-x-2">
+    <button v-on:click="decreaseCount" class="px-3 py-1 bg-red-600 rounded-lg text-white">-</button>
+    <p class="text-xl font-semibold">Counter: {{ count }}</p>
+    <button v-on:click="++count" class="px-3 py-1 text-white bg-green-500 rounded-lg">+</button>
+  </div>
+
+</div>
+</template>
+
+<style scoped>
+  
+</style>
---
+++ groupname-vue-app/src/test.vue
@@ -0,0 +1,35 @@
+<script setup>
+const msg ="<span class='italic'>Hello, v-text</span>";
+// const headingFont = 'text-4xl';
+const getToday=(x)=> {
+  return `test: ${msg.toUpperCase()}, ${new Date}`
+}
+const isShow = true;
+const score = 80;
+</script>
+ 
+<template>
+<div>
+    <section>
+      <h1>{{ getToday(msg) }}</h1>
+      <!-- v-text sample -->
+      <p v-text="msg.toUpperCase()"></p>
+      <p v-bind:class="headingFont">Hello, {{ msg.toUpperCase() }}, Goodbye</p>
+       <!-- v-html sample -->
+      <p v-html="msg"></p>
+       <!-- v-bind sample -->
+      <button class="bg-green-500 px-3 py-2 text-white rounded-lg disabled:bg-slate-500" v-if="isShow" v-bind:disabled="true">Preview</button>
+
+     <p v-if="score >= 80">A</p>
+     <p v-else-if="score < 80">B+</p>
+
+     <div>
+      <p v-for="x in 10">{{ x }}</p>
+     </div>
+    </section>
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
