298f5805d7484c205b60a1eacdb95a17220ab582
findmelily <pinny.pinngam@gmail.com>
2024-02-27 09:25:06 +0700

    aaa

---
+++ app-2/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+console.log("At App.vue (script setup)");
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello, Vue Components</h1>
+  </div>
+</template>
+
+<style scoped></style>
